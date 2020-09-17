c3409ad39a982ad4a5fbe277835dffca9da780b8
Vanessa Salvador <salvadoribanez.vanessa@gmail.com>
2020-09-17 09:52:43 +0200

    Set up form

--- src/Form/SortiesType.php
+++ src/Form/SortiesType.php
@@ -4,27 +4,38 @@ namespace App\Form;
 
 use App\Entity\Sorties;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class SortiesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('no_sortie')
-            ->add('nom')
-            ->add('datedebut')
-            ->add('duree')
-            ->add('datecloture')
-            ->add('nbinscriptionsmax')
-            ->add('descriptioninfos')
-            ->add('etatsortie')
-            ->add('urlPhoto')
-            ->add('organisateur')
-            ->add('lieux_no_lieu')
-            ->add('etats_no_etat')
-        ;
+            ->add('nom', TextType::class, [
+                'label' => 'Nom de la sortie'
+            ])
+            ->add('datedebut', DateTimeType::class, [
+                'label' => 'Date et heure de la sortie'
+            ])
+            ->add('datecloture', DateType::class, [
+                'label' => 'Date limite d\'inscription'
+            ])
+            ->add('duree', IntegerType::class, [
+                'label' => 'Durée'
+            ])
+            ->add('nbinscriptionsmax', IntegerType::class, [
+                'label' => 'Nombre de places'
+            ])
+            ->add('descriptioninfos', TextareaType::class, [
+                'label' => 'Description et infos'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
